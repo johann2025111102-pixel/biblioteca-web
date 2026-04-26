c9d8a80160b10963840f6499ca40f7e53e1cb855
Johann Muñiz Luna <johann.2025111102@metropoli.edu.mx>
2026-04-26 16:37:51 -0600

    Agregar base de datos corregida

--- database/biblioteca.sql
+++ database/biblioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-04-2026 a las 00:35:46
+-- Tiempo de generación: 27-04-2026 a las 00:37:18
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
